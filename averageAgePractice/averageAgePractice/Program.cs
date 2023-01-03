8ef875e1bfa2d0a3ad118194f25a10ebc2e37595
Austin Mays <110844923+tsundoku9509@users.noreply.github.com>
2023-01-02 19:19:56 -0600

    Add files via upload

---
+++ averageAgePractice/averageAgePractice/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace AverageAge
+{
+    public class Program
+    {
+        public void averageAge()
+        {
+            int ages = 0;
+            int num_of_ages = 3;
+        
+            while (ages < 120)
+            {
+                Console.WriteLine("Enter an age: ");
+                int age_01 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter an age: ");
+                int age_02 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter an age: ");
+                int age_03 = Convert.ToInt32(Console.ReadLine());
+                ages = age_01 + age_02 + age_03;
+                double average = ages / num_of_ages;
+                Console.WriteLine("The average is: " + average);
+            }
+
+        }
+        public static void Main(String[] args)
+        {
+            Program p = new Program();
+            p.averageAge();
+            Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ un-birthday_party/un-birthday_party/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+
+
+/**********************
+ * Name: un-birthday_party
+ * Author: Austin Mays
+ * Created: 09/07/2022
+ * Course: CIS 169 - C#
+ * Version: 1.0
+ * OS: macOS Big Sur
+ * IDE: Visual Studio 2022 Community
+ * Copyright: This is my own original work based on specifications issued by our instructor.
+ * Description: This program overall handles:
+ *              Input: The number of un-birthday guests, number of cupcakes, and how many each person should receive.
+ *              Output: A message saying "My Un-Birthday Guests, Enjoy!" + "Number of Cupcakes Per Person" + cupcakePerPerson.
+ * Academic Honesty: I attest thqat this is my original work. I have not used unauthorized source code, either modified or
+ * unmodified. I have not given other fellow students access to my program.
+**********************/
+
+
+
+namespace un_birthday_party
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numberPeople = 0;
+            int numberCupcakes = 0;
+            double cupcakePerPerson;
+
+            Console.WriteLine("Please enter the number of unbirthday people: ");
+            try
+            {
+                numberPeople = Convert.ToInt32(Console.ReadLine());
+
+            }
+           catch (FormatException x)
+            {
+                Console.WriteLine("Error in output...");
+                Console.WriteLine(x.Message);
+            }
+            Console.WriteLine("Please enter number of cupcakes: ");
+            try
+            {
+                numberCupcakes = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException y)
+            {
+                Console.WriteLine("Error in output...");
+                Console.WriteLine(y.Message);
+            }
+            cupcakePerPerson = numberPeople / numberCupcakes;
+            Console.WriteLine("My Un-Birthday Guests, Enjoy!" + " Number of cupcakes per person: " + cupcakePerPerson);
+            Console.ReadLine();
+
+        }
+    }
+}
+
